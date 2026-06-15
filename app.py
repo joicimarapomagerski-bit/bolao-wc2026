4cc656f875130853e092013e5571368da44d9b9c
joicimarapomagerski-bit <joicimarapomagerski@gmail.com>
2026-06-15 08:54:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -663,7 +663,7 @@ jogos_copa = carregar_jogos_do_banco()
 jogos_ativos = [j for j in jogos_copa if j["status"] != "FT"]
 jogos_finalizados = [j for j in jogos_copa if j["status"] == "FT"]
 
-aba_palpites, aba_finalizados, aba_ranking, aba_regras = st.tabs(["🔮 Agenda & Palpites", "📁 Jogos Finalizados", "📊 Ranking Geral", "📖 Como Funciona"])
+aba_palpites, aba_finalizados, aba_ranking, aba_regras = st.tabs(["🔮 Agenda & Palpites", "📁 Jogos Finalizados", "📊 Ranking Geral") #, "📖 Como Funciona"])
 
 with aba_palpites:
     if not jogos_ativos:
